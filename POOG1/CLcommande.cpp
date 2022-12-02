02243b893c8972f2351e6eb7f1edb733c0373449
liliaa22 <116554691+liliaa22@users.noreply.github.com>
2022-12-02 09:46:50 +0100

    commit Lilia

--- POOG1/CLcommande.cpp
+++ POOG1/CLcommande.cpp
@@ -1,2 +1,64 @@
 #include "pch.h"
 #include "CLcommande.h"
+namespace Comp_Mappage {
+
+void CLcommande::setid_client(int n){
+	this->id_client = n;
+}
+
+void CLcommande::setreference(System::String^ y) {
+	this->reference = y;
+}
+
+void CLcommande::setremise_globale_exceptionnel(float a) {
+	this->remise_globale_exceptionnel = a;
+}
+
+int CLcommande::getid_client(void) {
+	return this->id_client;
+}
+void CLcommande::setmontant(float z) {
+	this-> montant = z;
+}
+void CLcommande::setmoyen(System::String^ u) {
+	this->moyen = u;
+}
+void CLcommande::setquantite(float e) {
+	this->quantite = e;
+}
+
+void CLcommande::setremise_unitaire_exceptionnel(float o) {
+	this->remise_unitaire_exceptionnel = o;
+}
+
+void CLcommande::setid_article(int b) {
+	this->id_article = b;
+}
+
+System::String^ CLcommande::getreference(void) {
+	return this->reference;
+}
+float CLcommande::getremise_globale_exceptionnel(void) {
+	return this->remise_globale_exceptionnel;
+}
+
+int CLcommande::getid_client(int) {
+	return this->id_client;
+}
+
+float CLcommande::getmontant(void) {
+	return this->montant;
+}
+System::String^ CLcommande::getmoyen(void) {
+	return this->moyen;
+}
+float CLcommande::getquantite (void){
+	return this->quantite;
+}
+float CLcommande::getremise_unitaire_exceptionnel(void) {
+	return this->remise_unitaire_exceptionnel;
+}
+int CLcommande::getid_article(void) {
+	return this->id_article;
+}
+}
--- POOG1/CLcommande.h
+++ POOG1/CLcommande.h
@@ -1,41 +1,43 @@
 #pragma once
 #include "string"
+namespace Comp_Mappage {
 
-ref class CLcommande
-{
-public:
-	void setid_client(int);
-	void setreference(System::String^);
-	//void setdate_livraison(std::chrono::year_month_day);
-	void setremise_globale_exceptionnel(float);
-	int getid_client(void);
-	void setmontant(float);
-	void setmoyen(System::String^);
-	void setquantite(float);
-	void setremise_unitaire_exceptionnel(float);
-	void setid_article(int);
-	System::String getreference(void);
-	//std::chrono::year_month_day getdate_livraison(void);
-	float getremise_globale_exeptionnel(void);
-	int getid_client(int);
-	float getmontant(void);
-	System::String^ getmoyen(void);
-	float getquantite(void);
-	float getremise_unitaire_exceptionnel(void);
-	int getid_article(void);
+	ref class CLcommande
+	{
+	public:
+		void setid_client(int);
+		void setreference(System::String^);
+		//void setdate_livraison(std::chrono::year_month_day);
+		void setremise_globale_exceptionnel(float);
+		int getid_client(void);
+		void setmontant(float);
+		void setmoyen(System::String^);
+		void setquantite(float);
+		void setremise_unitaire_exceptionnel(float);
+		void setid_article(int);
+		System::String^ getreference(void);
+		//std::chrono::year_month_day getdate_livraison(void);
+		float getremise_globale_exceptionnel(void);
+		int getid_client(int);
+		float getmontant(void);
+		System::String^ getmoyen(void);
+		float getquantite(void);
+		float getremise_unitaire_exceptionnel(void);
+		int getid_article(void);
 
 
-protected:
-	int id_client;
-	System::String reference;
-	// std::chrono::year_month_day date_livraison;
-	float remise_globale_exceptionnel;
-	int id_client;
-	float montant;
-	System::String moyen;
-	float quantite;
-	float remise_unitaire_exceptionnel;
-	int id_article;
+	protected:
+		int id_client;
+		System::String^ reference;
+		// std::chrono::year_month_day date_livraison;
+		float remise_globale_exceptionnel;
+		int id_client;
+		float montant;
+		System::String^ moyen;
+		float quantite;
+		float remise_unitaire_exceptionnel;
+		int id_article;
 
-};
+	};
+}
 
