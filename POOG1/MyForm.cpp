eaf504a0448bb990a4db0fa2b7f9f79400b2e83b
Acemiouf <axel.eudier@viacesi.fr>
2022-12-02 16:30:36 +0100

    reorga

--- POOG1/MyForm.cpp
+++ POOG1/MyForm.cpp
@@ -1,4 +1,7 @@
+
 #include "MyForm.h"
+
+// Je fais juste un test pour voir le pull request
 
 using namespace System;
 using namespace System::Windows::Forms;
@@ -10,4 +13,19 @@ void Main(array<String^>^ args) {
     //WinformCDemo is your project name
     POOG1::MyForm form;
     Application::Run(% form);
+}
+
+void POOG1::MyForm::AjouterTexteCombobox()
+{
+    this->oDs = this->oSvc->voirListeLogin();
+    this->comboBox1->BeginUpdate();
+    for (int i = 0; i < this->oDs->Tables["Rsl"]->Rows->Count; i++) {
+        System::String^ tmp = "";
+        for (int j = 0; j < this->oDs->Tables["Rsl"]->Columns->Count; j++)
+            tmp = tmp + this->oDs->Tables["Rsl"]->Rows[i]->ItemArray[j]->ToString();
+
+        this->comboBox1->Items->Add(tmp);
+        //fonctionne mais c'est un format de naze
+    }
+    this->comboBox1->EndUpdate();
 }
